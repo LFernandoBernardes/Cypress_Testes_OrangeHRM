3cd839f4689abb8a85018c95108e7deac83f7f49
LFBernardes <borgesluizf@gmail.com>
2024-04-24 14:36:29 -0300

    Added selector list for login spec

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,21 +1,30 @@
 describe('Orange HRM Test', () => {
+
+  const selectorsList ={
+    usernameField: "[name='username']" ,
+    passwordField: "[name='password']" ,
+    loginButton: "[type='submit']" ,
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module" ,
+    wrongCredentialAlert: ".oxd-alert"
+  }
+
   it('Login Sucess ', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.title().should('be.equal', "OrangeHRM")
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get("[type='submit']").click()
-    cy.get('.oxd-topbar-header-breadcrumb-module').should('be.visible', "Dashboard")
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.sectionTitleTopBar).should('be.visible', "Dashboard")
     cy.location('pathname').should('be.equal', '/web/index.php/dashboard/index')
   })
 
   it('Login Fail ', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.title().should('be.equal', "OrangeHRM")
-    cy.get("[name='username']").type('Admin#@$@#')
-    cy.get("[name='password']").type('admin123')
-    cy.get("[type='submit']").click()
-    cy.get('.oxd-alert')
+    cy.get(selectorsList.usernameField).type('Admin#@$@#')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
     
   })
 
