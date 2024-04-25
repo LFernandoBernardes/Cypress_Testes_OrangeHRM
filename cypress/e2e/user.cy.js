ad7526fa7329d761480516e3d031039acffa9370
LFBernardes <borgesluizf@gmail.com>
2024-04-25 18:23:59 -0300

    add ramdon valeus chancejs

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -4,7 +4,9 @@ import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
 import MyInfoPage from '../pages/MyinfoPage'
 
+Chance = require ('chance')
 
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
@@ -17,7 +19,7 @@ describe('Orange HRM Test', () => {
         loginPage.loginWhithUser(userData.UserSuccess.username, userData.UserSuccess.password)
         dashboardPage.checkDashboardPage()
         menuPage.accessMyinfo()
-        myInfoPage.fillPersonalDetails('Seu', 'Madruga','Valdez')
+        myInfoPage.fillPersonalDetails(chance.first(), chance.last(), chance.last())
         myInfoPage.fillEmployeeDetails('1535','666','5846','2025-10-10')
         myInfoPage.fillStatus()
         myInfoPage.saveFor()
